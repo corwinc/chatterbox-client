ea3615f9f3c258d15b9089f3291393ba1abd8217
corwinc <corwin.crownover@gmail.com>
2016-08-08 20:28:37 -0700

    Chat rooms still remaining

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,65 +1,134 @@
 // Display messages retrieved from the server
 
-var app = {};
+String.prototype.replaceAll = function(search, replacement) {
+  var target = this;
+  return target.split(search).join(replacement);
+};
+
+var app = {
+  server: 'https://api.parse.com/1/classes/messages',
+  messages: []
+};
 
 var message = {
   username: 'shawndrost',
-  text: 'trololo',
+  text: 'trol\o<l{o',
   roomname: '4chan'
 };
 
-app.init = function () {};
+
 
 app.send = function (message) {
-	$.ajax({
-	  // This is the url you should use to communicate with the parse API server.
-	  url: 'https://api.parse.com/1/classes/messages',
-	  type: 'POST',
-	  data: JSON.stringify(message),
-	  contentType: 'application/json',
-	  success: function (data) {
-	    console.log('chatterbox: Message sent');
-	  },
-	  error: function (data) {
-	    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-	    console.error('chatterbox: Failed to send message', data);
-	  }
-	});
+  $.ajax({
+	// This is the url you should use to communicate with the parse API server.
+    url: this.server,
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) { 
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+		// See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
 };
 
 app.fetch = function() {
-	$.ajax({
-	  // This is the url you should use to communicate with the parse API server.
-	  url: undefined,
-	  type: 'GET',
-	  dataType: 'json',
-	  contentType: 'application/json',
-	  success: function (data) {
-	    console.log('chatterbox: Message retrieved');
-	    var result = 'hey!';
-	    return result;
-	  },
-	  error: function (data) {
-	    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-	    console.error('chatterbox: Failed to get message', data);
-	  }
-	});	
+  $.ajax({
+  // This is the url you should use to communicate with the parse API server.
+    url: this.server,
+    type: 'GET',
+    success: this.recieve,
+    error: function (data) {
+  // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to get message', data);
+    }
+  });	
 
 };
 
-app.addMessage = function() {
-	var result = app.fetch();
-			$('#main').append('<div>'+ result +'</div>');
+app.filterText = function(text) {
+  var r = new RegExp('&|<|>|"|\'|`|!|@|\\$|%|\\(|\\)|=|\\+|{|}|\\[|\\\\|\\]', 'g');
+  text = text.replace(r, '');
+  return text;
+};
 
-	// for (var i = 0; i < result.length; i++) {
-	// }
-}
+app.recieve = function (data) {
+	console.log(data);
+  $('#chats').empty();
+  data.results.forEach((el) => {
+    var filtered = app.filterReceivedObject(el);
+    app.addMessage(filtered);
+  });
+};
+
+app.filterReceivedObject = function (rObject) {
+  for (var key in rObject) {
+    rObject[key] = app.filterText(rObject[key]);
+  }
+  return rObject;
+};
+ 
+$('.username').on('click', function (e) {
+
+});
+
+app.addMessage = function(message) {
+  if (typeof message.username !== 'string') {
+     message.username = 'BADUSER';
+  } else {
+    var r = new RegExp('[^a-zA-Z0-9]', 'g');
+    message.username = message.username.replace(r , "");
+  }
+  var $span = $('<span></span>');
+  var $a = $('<a></a>');
+  $a.addClass('username');
+  $a.on('click', function (e) {
+    $(`.${message.username}`).toggleClass('friend');
+  });
+  $a.text(`${message.username} :`);
+
+  var $message = $('<span></span><br>');
+  $message.addClass(message.username);
+  $message.text(`${message.text}`);
+
+
+  $span.append($a);
+  $span.append($message);
+
+  $('#chats').append($span);
+};
+
+
 
 app.clearMessages = function () {
-	$('.message').remove();
+  $('#chats').empty();
 };
 
-app.addRoom = function() {};
+app.addRoom = function(roomName) {
+  $('#roomSelect').append(`<option value='${roomName}'>${roomName}</option>`);
+};
 
-app.addMessage();
-setInterval(app.addMessage, 3000);
+//##TODO## add roomname support to message sending
+app.init = function () {
+  var fetcher = app.fetch.bind(app);
+  setInterval(fetcher, 1000);
+
+  var $ourButton = $('<input id="submitButton" type="button" value="Submit"></input>');
+  $ourButton.on('click', function (e) {
+  
+    var message = $('#textInput').val();
+    var message = {
+      username: window.location.search.replace('username=', ""),
+      text: message,
+      roomname: '4chan'
+    };
+    app.send(message);
+  });
+  $('#form').append($ourButton);
+
+};
+
+$(document).ready(app.init);
